2002b4968a3c1d6e7eab4a1bf0e45222267ec371
olyllo <39379553+olyllo@users.noreply.github.com>
2020-07-27 23:11:53 +0300

    Update PermMissingElem.php

--- Task3/PermMissingElem.php
+++ Task3/PermMissingElem.php
@@ -1,1 +1,16 @@
-fsfsf
+// you can write to stdout for debugging purposes, e.g.
+// print "this is a debug message\n";
+
+function solution($A) {
+    // write your code in PHP7.0
+    
+    $length=count($A);
+    
+    $size = count($A) + 1;
+   
+    $total = ($size * ($size + 1)) / 2;// sum of consecutive integers from 1 to n is equal to n(n+1)/2
+
+    return  $total - array_sum($A);
+
+    
+}
